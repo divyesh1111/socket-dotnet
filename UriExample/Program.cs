2d04d66315f56e341b57463c8fb172ad4b53e7d3
divyesh1111 <divyeshhirpara000@gmail.com>
2026-03-26 12:17:41 -0700

    feat:Add initial project structure with .gitignore, solution, and example program

---
+++ UriExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        const string uriString = "https://www.fdu.edu/campuses/vancouver-campus/about-vancouver-campus/this-month/campus-updates/#ugtuition";
+
+        Uri canonicalUri = new Uri(uriString);
+
+        Console.WriteLine("Host: " + canonicalUri.Host);
+        Console.WriteLine("Path: " + canonicalUri.PathAndQuery);
+        Console.WriteLine("Fragment: " + canonicalUri.Fragment);
+    }
+}
